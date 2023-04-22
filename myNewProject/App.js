1d47338e441c89ec6526d8ca8d2c3973654f5af8
AntonAnatoliyovich <anton.shadura@gmail.com>
2023-04-22 20:11:53 +0300

    backgroundImage

--- myNewProject/App.js
+++ myNewProject/App.js
@@ -1,13 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import RegistrationScreen from './Screens/RegistrationScreen';
+// import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, View, Text } from 'react-native';
+// import RegistrationScreen from './Screens/RegistrationScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <RegistrationScreen />
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground 
+        source={require("./assets/images/backgroundImage.jpeg")} 
+        style={styles.image}
+      >
+        <Text>Open up App.js to start working on your app!</Text>
+      </ImageBackground>
       <StatusBar style="auto" />
+
+      {/* <RegistrationScreen/> */}
     </View>
   );
 }
@@ -15,8 +22,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    flex: 1,
+    width: '100%',
+    resizeMode: "cover",
+    justifyContent: "center",
+    }
 });
--- myNewProject/Screens/RegistrationScreen.jsx
+++ myNewProject/Screens/RegistrationScreen.jsx
@@ -1,27 +1,25 @@
-import React from "react";
-import { StyleSheet, ImageBackground } from 'react-native';
+// import React from 'react';
+// import { ImageBackground, StyleSheet, View } from 'react-native';
 
-function RegistrationScreen () {
-    return
-    <View style={styles.container}>
-        <ImageBackground 
-        source={require("../assets/images/backgroundImage.jpeg")}
-        style={styles.image}
-        ></ImageBackground>
-    </View>
-}
+// const RegistrationScreen = () => (
+//     <View style={styles.container}>
+//         <ImageBackground 
+//             source={require("../assets/images/backgroundImage.jpeg")} 
+//             style={styles.image}>
+//         </ImageBackground>
+//     </View>
+// )
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-    },
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//     },
+//     image: {
+//         flex: 1,
+//         width: '100%',
+//         resizeMode: "cover",
+//         justifyContent: "center",
+//     }
+// })
 
-    image: {
-        flex: 1,
-        resizeMode: "cover",
-        justifyContent: "center",
-    }
-})
-
-export default RegistrationScreen;
+// export default RegistrationScreen;
